7d78d7a2f4846d1eb700937182766cea9e136355
Matsuda <matsuda@MatsudaMacBook-Air.local>
2026-02-26 15:40:41 +0900

    Priorityモデルを作成

--- app/Models/Priority.php
+++ app/Models/Priority.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Priority extends Model
 {
-    //
+    protected $table = 'ai_tasks_M_priorities';
+
+    // 主キーの型を指定（デフォルトではBIGINT、ここではTINYINT）
+    // 書かなくてもいいが明示的に指定しておく
+    protected $keyType = 'int';
+
+    // マスターデータなので基本的に更新しない
+    protected $fillable = [
+        'name',
+        'level',
+        'color',
+    ];
+
+    // created_at, updated_atカラムを使わない
+    public $timestamps = false;
+
+    // 逆方向のリレーション
+    // このPriorityには複数のTaskが関連付けられる
+    // hasMany(関連モデル::class, 自分側の外部キー, 関連モデルの主キー(省略可))
+    public function tasks()
+    {
+        return $this->hasMany(Task::class, 'priority_id');
+    }
 }
