79fd790e1a9b7119065d3deb9d63a818905a6a62
Matsuda <matsuda@MatsudaMacBook-Air.local>
2026-03-11 11:54:34 +0900

    feat: AuthControllerを作成してルーティングを設定

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\AuthController;
 
+// ログイン画面の表示
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+
+// ログイン処理（フォーム送信時）
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
 Route::get('/settings',  [TaskController::class, 'settings'])->name('tasks.settings');
-
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login');
